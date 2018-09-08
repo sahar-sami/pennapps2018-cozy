584f49b5104559a6cc7eca0539c92e7489801b63
sahar-sami <31807245+sahar-sami@users.noreply.github.com>
2018-09-08 14:18:19 -0400

    updates

--- background.js
+++ background.js
@@ -15,4 +15,3 @@
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
-  });
--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
 window.onload = function (){
-    var risk = "mid";
-    document.getElementById('risk').innerHTML="<img id = 'dial' src = 'lowrisk.png'> This location has a " + risk + " risk of natural disasters.";
-    document.getElementById('dial').src=risk + "risk.png";
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'http://api.openhazards.com/GetEarthquakeProbability?q=San+Francisco,+CA&m=6.8&r=100', false);
 	//xhr.responseType = 'blob';
 	console.log(xhr.responseText);
+    xhr.send();
+    prob = earthquake(xhr.responseText);
+   // console.log(prob);
+    var risk = "mid";
+    displayRisk(risk);
+}
 
-xhr.send();}
+function earthquake(xml){
+    
+    return prob;
+}
+
+function displayRisk(risk){
+    document.getElementById('risk').innerHTML="<img id = 'dial' src = 'lowrisk.png'> This location has a " + risk + " risk of natural disasters.";
+    document.getElementById('dial').src=risk + "risk.png";
+}
