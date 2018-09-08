8e6a764a68aa42f359835f4aa75e4bc68e21c75f
sahar-sami <31807245+sahar-sami@users.noreply.github.com>
2018-09-08 12:52:17 -0400

    frontend update

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 window.onload = function (){
-    var risk = "high";
+    var risk = "mid";
     document.getElementById('risk').innerHTML="<img id = 'dial' src = 'lowrisk.png'> This location has a " + risk + " risk of natural disasters.";
     document.getElementById('dial').src=risk + "risk.png";
 }
