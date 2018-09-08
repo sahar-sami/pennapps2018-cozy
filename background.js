728beb00167502c29ab6ef5d2b5a4a3b7457f72b
sahar-sami <31807245+sahar-sami@users.noreply.github.com>
2018-09-07 23:31:17 -0400

    extension opens on housing websites

--- background.js
+++ background.js
@@ -10,9 +10,13 @@
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
+       conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'www.trulia.com'}
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'www.realtor.com'}
+          })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
