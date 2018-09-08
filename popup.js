9af2bd09ed3a8e3fcec20dfce58e26eba82b351b
MN-Hogwarts <mrinalinakhre.7@gmail.com>
2018-09-08 11:54:41 -0400

    HTTP request successful

--- popup.js
+++ popup.js
@@ -2,4 +2,9 @@ window.onload = function (){
     var risk = "high";
     document.getElementById('risk').innerHTML="<img id = 'dial' src = 'lowrisk.png'> This location has a " + risk + " risk of natural disasters.";
     document.getElementById('dial').src=risk + "risk.png";
-}
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://api.openhazards.com/GetEarthquakeProbability?q=San+Francisco,+CA&m=6.8&r=100', false);
+	//xhr.responseType = 'blob';
+	console.log(xhr.responseText);
+
+xhr.send();}
