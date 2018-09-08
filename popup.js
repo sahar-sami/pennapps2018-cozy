d47c3e7a17f4d7caaa66ba05e90a360d73fe568c
EeshaSharma <gulabi.usa@gmail.com>
2018-09-08 15:05:37 -0400

    adding a lil jquery

--- popup.js
+++ popup.js
@@ -26,3 +26,7 @@ function displayRisk(risk){
     document.getElementById('risk').innerHTML="<img id = 'dial' src = 'lowrisk.png'> This location has a " + risk + " risk of natural disasters.";
     document.getElementById('dial').src=risk + "risk.png";
 }
+
+$.get("https://www.mydomain.com/?url=https://www.google.com", function(response){
+	alert(response)
+});
